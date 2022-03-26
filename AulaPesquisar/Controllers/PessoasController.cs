a17d53828bc15481c12db30116d8b8178ce588b3
MATHEUS PITA <matheus.pita01@fatec.sp.gov.br>
2022-03-25 21:55:08 -0300

    aula

--- AulaPesquisar/Controllers/PessoasController.cs
+++ AulaPesquisar/Controllers/PessoasController.cs
@@ -9,12 +9,44 @@ namespace AulaPesquisar.Controllers
 
         public PessoasController(Contexto _db)
         {
-            db = _db;   
+            db = _db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string query, string itembusca)
         {
-            return View();
+            if (string.IsNullOrEmpty(query))
+            {
+
+                return View(db.PESSOAS.ToList());
+            }
+            else
+            {
+                switch (itembusca)
+                {
+                    default:
+                        return View(db.PESSOAS.Where(a => a.nome.Contains(query) ||
+               a.nome.Contains(query) ||
+               a.idade.Contains(query) ||
+               a.cpf.Contains(query) ||
+               a.rg.Contains(query)));
+                    case "Idade":
+                       return View(db.PESSOAS.Where(a => a.idade == query));
+                    case "CPF":
+                        return View(db.PESSOAS.Where(a => a.cpf == query));
+                    case "RG":
+                        return View(db.PESSOAS.Where(a => a.rg == query));
+                    case "Nome":
+                        return View(db.PESSOAS.Where(a => a.nome.Contains(query)));
+                   
+                    default:
+                        break;
+                }
+               
+
+
+
+
+            }
         }
     }
 }
